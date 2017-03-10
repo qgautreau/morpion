0e43a98ee64aaab0759ed7d2a43d4d66e80ea037
Quentin Gautreau <qgautreau@gmail.com>
2017-03-10 15:13:39 +0100

    fonctions jeu et tour

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
+var grid = [["","",""],
+["","",""],
+["","",""]];
+var grille = $("#grille");
+var cell = 0;
+var win = $("#win");
+var fin = false;
 
-$(document).ready(function(){
-    var grid = [["","",""],
-    ["","",""],
-    ["","",""]];
-    var grille = $("#grille");
-    var cell = 0;
-    var win = $("#win");
-    var fin = false;
+for(var i = 0; i < grid.length; i++){
+    for(var j = 0; j < grid[i].length; j++){
+        var divEl = $("<div>").attr({
+            class: "case",
+            id:"cell0" + cell++,
+            onclick:"jouer(this)"
+        }).appendTo(grille);
 
-    for(var i = 0; i < grid.length; i++){
-        for(var j = 0; j < grid[i].length; j++){
-            var divEl = $("<div>").attr({
-                class: "case",
-                id:"cell0" + cell++,
-                onclick:"jouer(this)"
-            }).appendTo(grille);
-
-        }
     }
-});
+}
+
+var nbTour = 0;
+var joueur;
+
+function tour(){
+    nbTour++;
+    if(nbTour % 2 == 0){
+        joueur = "O";
+    }
+    else{
+        joueur = "X";
+    }
+    return joueur;
+};
+
+function jouer(curntCell){
+    if($(curntCell).html() == "" && !fin){ //fin == false
+        $(curntCell).html(tour())
+    }
+
+};
